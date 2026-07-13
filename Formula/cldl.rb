9743e7c2b9ed2ac7605d485d7d0131c81720c194
Jussi Nokelainen <jussinoke@gmail.com>
2026-07-14 02:58:10 +0300

    Update description

--- Formula/cldl.rb
+++ Formula/cldl.rb
@@ -1,5 +1,5 @@
 class Cldl < Formula
-  desc "Todo CLI"
+  desc "Directory based cli todo lists"
   homepage "https://github.com/jussinokelainen/cldl"
   url "https://github.com/jussinokelainen/cldl/archive/refs/tags/v0.2.1.tar.gz"
   sha256 "ed41bc1aaf5d79c189db7a220266e4bf4d87f19d605d5ace0942bdafb037ed19"
