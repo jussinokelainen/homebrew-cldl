e1aa2abbdb7799a2c6edc787d3465404b42dc345
Jussi Nokelainen <jussinoke@gmail.com>
2026-07-14 02:09:46 +0300

    update to version 0.2.0

--- Formula/cldl.rb
+++ Formula/cldl.rb
@@ -1,8 +1,8 @@
 class Cldl < Formula
   desc "Todo CLI"
   homepage "https://github.com/jussinokelainen/cldl"
-  url "https://github.com/jussinokelainen/cldl/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "8e7c510772da3c2efe889c8de1926a1436b71152c89ed60ce50879d4ca2c7c44"
+  url "https://github.com/jussinokelainen/cldl/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "855d19ca0812b06936cf972c126bebb9f099ce030ac6bee7538198a7e95a7a13"
   license "MIT"
 
   depends_on "go" => :build
