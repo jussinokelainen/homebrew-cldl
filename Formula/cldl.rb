2489f00dac563b51099607ecbaf9097990f276eb
Jussi Nokelainen <jussinoke@gmail.com>
2026-07-16 03:56:46 +0300

    Updated to version v0.2.5

--- Formula/cldl.rb
+++ Formula/cldl.rb
@@ -1,8 +1,8 @@
 class Cldl < Formula
   desc "Directory based cli todo lists"
   homepage "https://github.com/jussinokelainen/cldl"
-  url "https://github.com/jussinokelainen/cldl/archive/refs/tags/v0.2.4.tar.gz"
-  sha256 "f4e3c22c5d25ee460436fc001e0772915cf9f8594c49c779a7b79f9856d7c9e3"
+  url "https://github.com/jussinokelainen/cldl/archive/refs/tags/v0.2.5.tar.gz"
+  sha256 "6b4de510bfe504338dfaf967c4412aa8b00ee7f4ebded4b12cdd0b3848e20473"
   license "MIT"
 
   depends_on "go" => :build
